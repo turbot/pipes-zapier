d8c5d65d8fd02cc4684982f6889c06986cfcdc80
Subhajit Mondal <subhajit@turbot.com>
2023-03-24 11:28:59 +0530

    Update error handling to return proper error message if a bad query passed in the input

--- triggers/error-handler.js
+++ triggers/error-handler.js
@@ -9,6 +9,9 @@ const handleHTTPError = (response, z) => {
   else if (response.status === 429) {
     throw new ThrottledError(response.json.detail, 600);
   }
+  else if (response.status === 500) {
+    throw new z.errors.HaltedError(response.json.detail, response.json.status);
+  }
   else if (response.status >= 400) {
     throw new z.errors.Error(response.json.detail, response.json.status);
   }
